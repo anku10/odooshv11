11fb6ab04b1fc22bd57053ffed4ab6f6b8ec8681
Deepak Nayak <deepak_nayak@seologistics.com>
2020-09-03 16:52:06 +0530

    delete

--- bank_account_archive/models/account_journal.py
+++
@@ -1,8 +0,0 @@
-
-from odoo import fields, models
-
-class AccountJournal(models.Model):
-
-    _inherit = 'account.journal'
-
-    active = fields.Boolean(default=True)
--- unique_sku_alert/models/product.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError
-
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-    _description = 'Product Template'
